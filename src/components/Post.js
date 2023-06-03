854f4b8a392e6b4d79832c7150cffc0cf6154093
rolandsobczak <rolandsobczak@icloud.com>
2023-06-03 20:46:58 +0200

    feat: backend connected

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,87 +1,106 @@
-import { View, Text, Image} from 'react-native'
-import { LeafActiveSvg, LeafInactiveSvg, ShareSvg, CommentSvg } from '../SvgIcons';
+import { View, Text, Image } from "react-native";
+import {
+	LeafActiveSvg,
+	LeafInactiveSvg,
+	ShareSvg,
+	CommentSvg,
+} from "../SvgIcons";
 
-export function Post() {
-    const data = {
-      name: "Maciek",
-      time: "20 minut temu",
-      image: require("../../assets/images/example-post.png"),
-      leaves: 673,
-      comments: 23,
-      liked: true,
-    };
-  
-    return (
-      <View style={{ width: "100%", paddingHorizontal: 28, paddingTop: 40 }}>
-        <Text
-          style={{
-            fontSize: 18,
-            fontFamily: "Helvetica",
-            color: "#000000",
-            lineHeight: 18,
-          }}
-        >
-          {data.name}
-        </Text>
-        <Text
-          style={{
-            fontSize: 14,
-            fontFamily: "HelveticaLight",
-            color: "#A3A3A3",
-            lineHeight: 14,
-            marginTop: 2
-          }}
-        >
-          {data.time}
-        </Text>
-        <Image
-          style={{
-            height: undefined,
-            width: "100%",
-            aspectRatio: 3 / 2,
-            marginTop: 5,
-            borderRadius: 10,
-          }}
-          source={data.image}
-        ></Image>
-        <View
-          style={{
-            width: "100%",
-            flexDirection: "row",
-            justifyContent: "space-between",
-            alignItems: "center",
-            marginTop: 16,
-          }}
-        >
-          <View style={{ flexDirection: "row", alignItems:'center' }}>
-            <Text
-              style={{
-                fontSize: 16,
-                fontFamily: "Helvetica",
-                color: "#000",
-              }}
-            >
-              {data.leaves}
-            </Text>
-            {data.liked ? (
-              <LeafActiveSvg style={{ marginHorizontal: 8 }} />
-            ) : (
-              <LeafInactiveSvg style={{ marginHorizontal: 8 }} />
-            )}
-            <Text
-              style={{
-                fontSize: 16,
-                fontFamily: "Helvetica",
-                color: "#000",
-                marginLeft: 16
-              }}
-            >
-              {data.comments}
-            </Text>
-            <CommentSvg style={{ marginHorizontal: 8 }} />
-          </View>
-          <ShareSvg />
-        </View>
-      </View>
-    );
-  }
+export function Post({ item }) {
+	const data = {
+		name: item.user,
+		time: item.created_at,
+		image: require("../../assets/images/example-post.png"),
+		leaves: 673,
+		comments: 23,
+		liked: true,
+	};
+	const parsedDatetime = new Date(data.time);
+
+	// Get the current time
+	const currentTime = new Date();
+
+	// Calculate the time difference in milliseconds
+	const timeDiff = currentTime.getTime() - parsedDatetime.getTime();
+
+	// Calculate the time difference in minutes
+	const minutesAgo = Math.floor(timeDiff / (1000 * 60));
+
+	// Format the result
+	const formattedTime =
+		minutesAgo === 0 ? "przed chwilą" : `${minutesAgo} minut temu`;
+
+	return (
+		<View style={{ width: "100%", paddingHorizontal: 28, paddingTop: 40 }}>
+			<Text
+				style={{
+					fontSize: 20,
+					fontFamily: "HelveticaBold",
+					color: "#000000",
+					lineHeight: 20,
+				}}
+			>
+				{data.name}
+			</Text>
+			<Text
+				style={{
+					fontSize: 14,
+					fontFamily: "Helvetica",
+					color: "#A3A3A3",
+					lineHeight: 14,
+				}}
+			>
+				{formattedTime}
+			</Text>
+			<Image
+				style={{
+					height: undefined,
+					width: "100%",
+					aspectRatio: 3 / 2,
+					marginTop: 5,
+					borderRadius: 10,
+				}}
+				source={data.image}
+			></Image>
+			<View
+				style={{
+					width: "100%",
+					flexDirection: "row",
+					justifyContent: "space-between",
+					alignItems: "center",
+					marginTop: 16,
+				}}
+			>
+				<View style={{ flexDirection: "row" }}>
+					<Text
+						style={{
+							fontSize: 18,
+							fontFamily: "Helvetica",
+							color: "#000",
+							lineHeight: 18,
+						}}
+					>
+						{data.leaves}
+					</Text>
+					{data.liked ? (
+						<LeafActiveSvg style={{ marginHorizontal: 8 }} />
+					) : (
+						<LeafInactiveSvg style={{ marginHorizontal: 8 }} />
+					)}
+					<Text
+						style={{
+							fontSize: 18,
+							fontFamily: "Helvetica",
+							color: "#000",
+							lineHeight: 18,
+						}}
+					>
+						{data.comments}
+					</Text>
+					<CommentSvg style={{ marginHorizontal: 8 }} />
+				</View>
+				<ShareSvg />
+			</View>
+		</View>
+	);
+}
