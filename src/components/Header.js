a1ba7ed55a8a108cbfb40b2e5c1d2d48e670e87d
czolojd3000 <masllo.maslo@gmail.com>
2023-07-08 16:29:30 +0200

    refactor: extracting styles from Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,58 +1,67 @@
-import { View, Image, Text, TouchableOpacity } from "react-native";
+import { View, Image, Text, TouchableOpacity, StyleSheet } from "react-native";
 import { BackArrowSvg, HeaderLogoSvg } from "./SvgIcons";
 
 export default function Header() {
-  return (
-    <View
-      style={{
+    const image = require("../../assets/images/example-profile-avatar.png");
+
+    return (
+        <View style={styles.headerContainer}>
+            <HeaderLogoSvg />
+            <View style={{ flexDirection: "row-reverse" }}>
+                <Image source={image} />
+                <View style={{ justifyContent: "center", alignItems: "flex-end", marginRight: 10 }}>
+                    <Text style={styles.headerProfileName}>Marta</Text>
+                    <Text style={styles.headerShowProfile}>Zobacz swój profil</Text>
+                </View>
+            </View>
+        </View>
+    );
+}
+
+export function HeaderBackArrow({ title, onPress }) {
+    return (
+        <View
+            style={{
+                width: "100%",
+                height: 110,
+                alignItems: "center",
+                flexDirection: "row",
+                paddingHorizontal: 28,
+            }}
+        >
+            <TouchableOpacity onPress={onPress}>
+                <BackArrowSvg />
+            </TouchableOpacity>
+
+            <Text
+                style={{
+                    fontSize: 24,
+                    fontFamily: "HelveticaBold",
+                    color: "#000000",
+                    marginLeft: 28,
+                }}
+            >
+                {title}
+            </Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    headerContainer: {
         width: "100%",
         height: 110,
         justifyContent: "space-between",
         alignItems: "center",
         flexDirection: "row",
         paddingHorizontal: 28,
-      }}
-    >
-      <HeaderLogoSvg />
-
-      <View style={{ flexDirection: "row-reverse" }}>
-        <Image
-          source={require("../../assets/images/example-profile-avatar.png")}
-        />
-        <View style={{ justifyContent: "center", alignItems: "flex-end", marginRight: 10 }}>
-          <Text style={{color: '#232323', fontFamily: 'Helvetica'}}>Marta</Text>
-          <Text style={{color: '#797979', fontFamily: 'HelveticaLight'}}>Zobacz swój profil</Text>
-        </View>
-      </View>
-    </View>
-  );
-}
-
-export function HeaderBackArrow({ title, onPress }) {
-  return (
-    <View
-      style={{
-        width: "100%",
-        height: 110,
-        alignItems: "center",
-        flexDirection: "row",
-        paddingHorizontal: 28,
-      }}
-    >
-      <TouchableOpacity onPress={onPress}>
-        <BackArrowSvg />
-      </TouchableOpacity>
-      
-      <Text
-        style={{
-          fontSize: 24,
-          fontFamily: "HelveticaBold",
-          color: "#000000",
-          marginLeft: 28
-        }}
-      >
-        {title}
-      </Text>
-    </View>
-  );
-}
+    },
+    headerProfileName: {
+        color: "#232323",
+        fontFamily: "Helvetica",
+    },
+    headerShowProfile: {
+        color: "#797979",
+        fontFamily: "HelveticaLight",
+    },
+});
